3e1398e499d104d6601879b1775b54eb875fc17a
cristiancrakl2 <clmoralesc@ufpso.edu.co>
2025-09-23 16:39:39 -0500

    haciendo una cosita para probar

--- src/Logica/crudCliente_Logica.java
+++ src/Logica/crudCliente_Logica.java
@@ -5,9 +5,9 @@ import javax.swing.JOptionPane;
 public class crudCliente_Logica {
 
     Nodo primero;
-    
+
     int[][] horarioMesa = new int[3][6];
-    
+
     public void ingresarCliente(Persona cliente) {
 
         System.out.println("entro aqui");
@@ -21,13 +21,12 @@ public class crudCliente_Logica {
         }
         JOptionPane.showMessageDialog(null, "cliente: " + primero.getPersona().getNombre() + " ingresado con exito"
                 + "  a las: " + primero.getPersona().getHoraIngreso());
-        
+
         horarioMesa[cliente.getMesaPosicion()][cliente.getDiaSemana()] += 1;
     }
 
     public void consultar() {
 
-        System.out.println("entro aqui");
         Nodo temporal = primero;
         if (temporal == null) {
             JOptionPane.showMessageDialog(null, "Lista vacia");
@@ -40,7 +39,7 @@ public class crudCliente_Logica {
             }
         }
     }
-    
+
     public void totalClientesSemanales() {
         int totalClientes = 0;
         for (int i = 0; i < horarioMesa.length; i++) {
@@ -48,57 +47,56 @@ public class crudCliente_Logica {
                 totalClientes += horarioMesa[i][j];
             }
         }
-        JOptionPane.showMessageDialog(null, 
-            "El total de clientes en la semana es: " + totalClientes);
+        JOptionPane.showMessageDialog(null,
+                "El total de clientes en la semana es: " + totalClientes);
     }
-    
+
     public void totalClientesPorDiaSemana() {
         for (int j = 0; j < horarioMesa[0].length; j++) {
-           int totalDia = 0;
-           for (int i = 0; i < horarioMesa.length; i++) {
-            totalDia += horarioMesa[i][j];
-        }
-        JOptionPane.showMessageDialog(null, 
-            "Total de clientes en el día " + (j + 1) + ": " + totalDia);
+            int totalDia = 0;
+            for (int i = 0; i < horarioMesa.length; i++) {
+                totalDia += horarioMesa[i][j];
+            }
+            JOptionPane.showMessageDialog(null,
+                    "Total de clientes en el día " + (j + 1) + ": " + totalDia);
         }
     }
-    
+
     public void totalClientesPorMesa() {
-    for (int i = 0; i < horarioMesa.length; i++) { 
-        int totalMesa = 0;
-
-        for (int j = 0; j < horarioMesa[i].length; j++) { 
-            totalMesa += horarioMesa[i][j];
-        }
-        
-        JOptionPane.showMessageDialog(null, 
-            "Total de clientes en la mesa " + (i + 1) + " = " + totalMesa);
-
-    }
-    }
-    
-    public void diaConMasClientes() {
-    int maximoDia = -1;
-    
-    int maximoClientes = -1;
-
-    for (int j = 0; j < horarioMesa[0].length; j++) { 
-        int totalDia = 0;
-
         for (int i = 0; i < horarioMesa.length; i++) {
-            totalDia += horarioMesa[i][j];
-        }
+            int totalMesa = 0;
 
-        if (totalDia > maximoClientes) {
-            maximoClientes = totalDia;
-            maximoDia = j;
+            for (int j = 0; j < horarioMesa[i].length; j++) {
+                totalMesa += horarioMesa[i][j];
+            }
+
+            JOptionPane.showMessageDialog(null,
+                    "Total de clientes en la mesa " + (i + 1) + " = " + totalMesa);
+
         }
     }
 
-    JOptionPane.showMessageDialog(null, 
-        "El día con más clientes es el día " + (maximoDia + 1) + 
-        " con un total de " + maximoClientes + " clientes.");
-}
+    public void diaConMasClientes() {
+        int maximoDia = -1;
 
+        int maximoClientes = -1;
+
+        for (int j = 0; j < horarioMesa[0].length; j++) {
+            int totalDia = 0;
+
+            for (int i = 0; i < horarioMesa.length; i++) {
+                totalDia += horarioMesa[i][j];
+            }
+
+            if (totalDia > maximoClientes) {
+                maximoClientes = totalDia;
+                maximoDia = j;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null,
+                "El día con más clientes es el día " + (maximoDia + 1) +
+                        " con un total de " + maximoClientes + " clientes.");
+    }
 
 }
